78ec2317c9a9964d22e42f5e262d8e595715ef2a
Armando Arce <armando.arce@gmail.com>
2022-10-11 14:04:05 -0600

    Delete Tutorial_06 directory

--- Tutorial_06/components/global/HeaderView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="row"  style="margin-top: 15px">
-    <button disabled="disabled" class="twelve columns button-primary">
-      Amazon Top 50 Bestselling Books
-    </button>
-  </div>
-</template>
