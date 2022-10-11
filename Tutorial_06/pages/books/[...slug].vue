9a5ed95f992266dc428da68ca6742ebb2d14b6e3
Armando Arce <armando.arce@gmail.com>
2022-10-11 14:04:57 -0600

    Add files via upload

---
+++ Tutorial_06/pages/books/[...slug].vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <HeaderView />
+    <div class="row">
+      <div class="three columns">
+      </div>
+      <div class="six columns">
+        <h4>{{book.name}}</h4>
+	     by {{book.author}}
+	     User Rating: {{book.userRating}}; Year: {{book.year}};
+	     Price: {{book.price}}; Genere: {{book.genre}}
+	    </div>
+	    <div class="two columns"></div>
+    </div>
+    <FooterView />
+  </div>
+</template>
+<script setup>
+	const route = useRoute()
+  const books = await queryContent('bestsellers').only('items').findOne()
+	const book = books.items.find(book => book.num == route.params.slug)
+</script>
---
+++ Tutorial_06/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+   <HeaderView />
+   <h3 style="margin-top: 15px">Amazon Top 50 Bestselling Books 2009 - 2019</h3>
+     <p>Dataset on Amazon's Top 50 bestselling books from 2009 to 2019. Contains 550 books, data has been categorized into fiction and non-fiction using Goodreads</p>
+   <ul>
+     <li v-for="book in books" :key="book.num">
+       <NuxtLink :to="`/books/${book.num}`">{{book.name}}</NuxtLink>
+     </li>
+   </ul>
+   <FooterView />
+ </div>
+</template>
+<script setup>
+  const data = await queryContent('bestsellers').only('items').findOne()
+	const books = data.items
+	console.log(books)
+</script>
